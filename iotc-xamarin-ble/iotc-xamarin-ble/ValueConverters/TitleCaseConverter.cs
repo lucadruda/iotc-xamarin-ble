f0ed18426086d9f5d545ebb30cc2460f5e3ad87b
user154610 <user154610@92-63-141-222.rdns.melbourne.co.uk>
2019-07-03 16:21:47 +0100

    fix iOS appearance

---
+++ iotc-xamarin-ble/iotc-xamarin-ble/ValueConverters/TitleCaseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace iotc_xamarin_ble.ValueConverters
+{
+    public class TitleCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = value as string;
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            return textInfo.ToTitleCase(text.ToLower());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
