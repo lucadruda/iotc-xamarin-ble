7110df6bc2bc89fe0df577ae6d69431e8711c665
ludruda <ludruda@microsoft.com>
2019-08-12 16:03:19 +0200

    add wifi instructions

--- iotc-xamarin-ble/iotc-xamarin-ble.iOS/Services/WiFiManager.cs
+++ iotc-xamarin-ble/iotc-xamarin-ble.iOS/Services/WiFiManager.cs
@@ -1,7 +1,12 @@
 ﻿using Foundation;
 using iotc_xamarin_ble.iOS.Services;
 using iotc_xamarin_ble.Services;
+using NetworkExtension;
 using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using SystemConfiguration;
 using UIKit;
 using Xamarin.Forms;
 
@@ -17,12 +22,47 @@ namespace iotc_xamarin_ble.iOS.Services
 
         public string GetConnectedAp()
         {
-            return "";
+            //foreach (var network in NEHotspotHelper.SupportedNetworkInterfaces)
+            //{
+            //    if (!string.IsNullOrEmpty(network.Ssid))
+            //        return network.Ssid;
+            //}
+            //return string.Empty; //check for iOS 11+
+            NSDictionary dict;
+            var status = CaptiveNetwork.TryCopyCurrentNetworkInfo("en0", out dict);
+            if (status == StatusCode.NoKey)
+            {
+                return string.Empty;
+            }
+
+            var bssid = dict[CaptiveNetwork.NetworkInfoKeyBSSID];
+            var ssid = dict[CaptiveNetwork.NetworkInfoKeySSID];
+            var ssiddata = dict[CaptiveNetwork.NetworkInfoKeySSIDData];
+
+            return ssid.ToString();
         }
 
         public string GetCurrentIp()
         {
-            return "";
+            String ipAddress = "";
+
+            foreach (var netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (netInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                    netInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    foreach (var addrInfo in netInterface.GetIPProperties().UnicastAddresses)
+                    {
+                        if (addrInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            ipAddress = addrInfo.Address.ToString();
+
+                        }
+                    }
+                }
+            }
+
+            return ipAddress;
         }
 
         public void ReceiveBroadcast()
@@ -32,7 +72,14 @@ namespace iotc_xamarin_ble.iOS.Services
 
         public void Scan()
         {
-            var WiFiURL = new NSUrl("prefs:root=WIFI");if(UIApplication.SharedApplication.CanOpenUrl(WiFiURL)){   //Pre iOS 10    UIApplication.SharedApplication.OpenUrl(WiFiURL);}else{   //iOS 10    UIApplication.SharedApplication.OpenUrl(new NSUrl("App-Prefs:root=WIFI"));}
+            var WiFiURL = new NSUrl("prefs:root=WIFI");
+            if (UIApplication.SharedApplication.CanOpenUrl(WiFiURL))
+            {   //Pre iOS 10
+                UIApplication.SharedApplication.OpenUrl(WiFiURL);
+            }
+            else
+            {   //iOS 10    UIApplication.SharedApplication.OpenUrl(new NSUrl("App-Prefs:root=WIFI"));
+            }
 
         }
     }
